04c693ae9a352c64daf0813e319a34a808dde5a2
Jason Roberts <cshokie@gmail.com>
2026-01-15 17:53:58 -0500

    modified index to have an explicit check for production.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if(process.env.NODE_ENV == 'production'){
+if(process.env.NODE_ENV === 'production'){
 	// Express will serve up production assets
 	// like our main.js file, or main.css file!
 	app.use(express.static('client/build'));
